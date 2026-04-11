6904b11fc7ca741f4515eae8aa701adac111ea72
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-04-11 15:40:52 +0900

    feat: rewrite SourceDocViewerComponent to read from report_data

--- test/components/source_doc_viewer_component_test.rb
+++ test/components/source_doc_viewer_component_test.rb
@@ -1,15 +1,21 @@
 require "test_helper"
 
 class SourceDocViewerComponentTest < ViewComponent::TestCase
-  test "renders empty registry transcript message when no data" do
-    property = properties(:safe_apartment)
-    render_inline(SourceDocViewerComponent.new(property: property))
-    assert_text "등기부등본 데이터가 없습니다"
+  test "renders empty message when no report provided" do
+    render_inline(SourceDocViewerComponent.new(report: nil))
+    assert_text "분석을 먼저 실행해주세요"
   end
 
   test "renders disclaimer" do
-    property = properties(:safe_apartment)
-    render_inline(SourceDocViewerComponent.new(property: property))
+    report = rights_analysis_reports(:safe_apartment_report)
+    render_inline(SourceDocViewerComponent.new(report: report))
     assert_text "매각물건명세서 비고란을 직접 확인하세요"
+  end
+
+  test "renders error message when extraction failed" do
+    report = rights_analysis_reports(:safe_apartment_report)
+    report.report_data = { "analysis_status" => "extraction_failed" }
+    render_inline(SourceDocViewerComponent.new(report: report))
+    assert_text "분석 데이터를 구조화하는 데 실패했습니다"
   end
 end
