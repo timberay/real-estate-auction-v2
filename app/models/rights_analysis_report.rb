e710794e2988467b4c06c41e5f69f43f9da8a90d
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-04-11 16:23:54 +0900

    feat: add backward-compatible data helpers to RightsAnalysisReport

--- app/models/rights_analysis_report.rb
+++ app/models/rights_analysis_report.rb
@@ -6,4 +6,16 @@ class RightsAnalysisReport < ApplicationRecord
 
   validates :user_id, uniqueness: { scope: :property_id }
   validates :analyzed_at, presence: true
+
+  def effective_tenants
+    report_data&.dig("calculated", "tenants") || report_data&.dig("tenants") || []
+  end
+
+  def effective_rights_timeline
+    report_data&.dig("llm_raw", "rights_timeline") || report_data&.dig("rights_timeline") || []
+  end
+
+  def discrepancies
+    report_data&.dig("discrepancies") || []
+  end
 end
