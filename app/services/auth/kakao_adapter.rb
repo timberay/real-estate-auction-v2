9fca34c69985e7ba34c5601c5733a7114f1376f2
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-04-22 16:35:16 +0900

    feat(auth): KakaoAdapter maps email_verified

--- app/services/auth/kakao_adapter.rb
+++ app/services/auth/kakao_adapter.rb
@@ -11,7 +11,7 @@ module Auth
         provider: PROVIDER,
         uid: @auth_hash["uid"].to_s,
         email: @auth_hash.dig("info", "email"),
-        email_verified: nil,
+        email_verified: @auth_hash.dig("extra", "raw_info", "kakao_account", "is_email_verified"),
         name: @auth_hash.dig("info", "name"),
         avatar_url: @auth_hash.dig("info", "image")
       )
--- test/services/auth/kakao_adapter_test.rb
+++ test/services/auth/kakao_adapter_test.rb
@@ -25,4 +25,31 @@ class Auth::KakaoAdapterTest < ActiveSupport::TestCase
     assert_nil profile.email
     assert_equal "익명", profile.name
   end
+
+  test "maps kakao_account.is_email_verified true" do
+    auth_hash = OmniAuth::AuthHash.new(
+      "provider" => "kakao", "uid" => "1",
+      "info"  => { "email" => "a@b.com", "name" => "X", "image" => nil },
+      "extra" => { "raw_info" => { "kakao_account" => { "is_email_verified" => true } } }
+    )
+    assert_equal true, Auth::KakaoAdapter.new(auth_hash).to_profile.email_verified
+  end
+
+  test "maps kakao_account.is_email_verified false" do
+    auth_hash = OmniAuth::AuthHash.new(
+      "provider" => "kakao", "uid" => "2",
+      "info"  => { "email" => "a@b.com", "name" => "X", "image" => nil },
+      "extra" => { "raw_info" => { "kakao_account" => { "is_email_verified" => false } } }
+    )
+    assert_equal false, Auth::KakaoAdapter.new(auth_hash).to_profile.email_verified
+  end
+
+  test "kakao_account missing → email_verified is nil" do
+    auth_hash = OmniAuth::AuthHash.new(
+      "provider" => "kakao", "uid" => "3",
+      "info"  => { "email" => nil, "name" => "익명", "image" => nil },
+      "extra" => { "raw_info" => {} }
+    )
+    assert_nil Auth::KakaoAdapter.new(auth_hash).to_profile.email_verified
+  end
 end
