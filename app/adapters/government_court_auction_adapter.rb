0c879368fd845b7b350f9ffee3018ad86b8b8a13
Tonny Donghwi Kim <tonny.kim@timberay.com>
2026-05-04 21:31:47 +0900

    feat: add search_case to GovernmentCourtAuctionAdapter
    Delegates to CaseSearchClient with rate-limit throttle.
    Returns raw API data; parsing happens in CaseSearchService.

--- app/adapters/government_court_auction_adapter.rb
+++ app/adapters/government_court_auction_adapter.rb
@@ -2,6 +2,7 @@ class GovernmentCourtAuctionAdapter < CourtAuctionAdapter
   def initialize
     @browser_client = CourtAuction::BrowserClient.new
     @criteria_search_client = CourtAuction::CriteriaSearchClient.new
+    @case_search_client = CourtAuction::CaseSearchClient.new
     @parser = CourtAuction::ResponseParser.new
     @rate_limiter = CourtAuction::RateLimiter.new
   end
@@ -9,5 +10,10 @@ class GovernmentCourtAuctionAdapter < CourtAuctionAdapter
   def search_by_criteria(region_code:, max_price:, max_items: 100)
     @rate_limiter.throttle
     @criteria_search_client.search_all(region_code: region_code, max_price: max_price, max_items: max_items)
+  end
+
+  def search_case(court_code:, case_number:)
+    @rate_limiter.throttle
+    @case_search_client.search(court_code: court_code, case_number: case_number)
   end
 end
